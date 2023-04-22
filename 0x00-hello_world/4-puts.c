0008d612d657dc02a610b98bae7305d416bec583
logykhaled <khaledlogyn@gmail.com>
2023-04-22 12:41:43 -0700

    add file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ * puts - print a string to the standar output
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
